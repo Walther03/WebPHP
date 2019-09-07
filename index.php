b8a9127808f0b7f8e9ba542c1bee7018368d6267
Walther03 <waportuguezo@gmail.com>
2019-09-07 10:27:20 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>  
+<head>  
+<title>Mi primera página el PHP.</title>   
+</head>  
+
+<body> 
+
+Mi nombre es:  
+
+<?php  
+$nombre = "Antonio"; 
+$apellido1 = "García"; 
+$apellido2 = "Pérez"; 
+
+echo "$nombre $apellido1 $apellido2"; 
+
+?> 
+</body> 
+
+</html>
